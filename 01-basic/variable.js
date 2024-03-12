c82ea91447266dce13490170882861bcc462e8c2
rrsagar06 <rrsagar06@gmail.com>
2024-03-12 13:01:31 +0000

    coverstion data types

--- 01-basic/variable.js
+++ 01-basic/variable.js
@@ -2,4 +2,10 @@ const birtday = "19/08/1195";
 let u_name= "rahul";
 let u_age = 30;
 
-console.table([birtday, u_name, u_age]);
+//console.table([birtday, u_name, u_age]);
+
+// string converstion
+
+let someNumber = 35
+let stringNumber = String(someNumber);
+console.log(typeof(stringNumber));
